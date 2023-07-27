2b971345e8d8b441d00f242c036be7b186b1bdc2
Violeta <holasoyviole@gmail.com>
2023-07-27 20:40:19 -0300

    dotenv configurado

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,7 +1,14 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { environment } from './app/environments/environment';
 import { AppModule } from './app/app.module';
 
+if ( environment.production ) {
+  enableProdMode();
+}
+
+import 'dotenv/config';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
