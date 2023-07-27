944757fced7591a6576bd58c09f4fa921a6f7201
Violeta <holasoyviole@gmail.com>
2023-07-27 20:41:35 -0300

    service iniciado

--- Client/src/app/config.service.ts
+++ Client/src/app/config.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Countdown } from './countdown';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConfigService {
 
-  constructor() { }
+  private apiCountdownUrl = 'https://www.whenisthenextmcufilm.com/';
+  private publicKey = 'process.env.PUBLIC_KEY';
+  private privateKey = 'process.env.PRIVATE_KEY';
+  private apiComicUrl = 'developer.marvel.com';
+
+  constructor(private http: HttpClient) { }
+
+  getNextMovie(): Observable<Countdown[]> {
+    return this.http
+      .get<Countdown[]>(this.apiCountdownUrl)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: any) {
+    return throwError(error);
+  }
 }
+
